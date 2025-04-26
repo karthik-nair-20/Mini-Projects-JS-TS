57ab69b26f64b4d6e7dcf4f498da7196d7d54820
kxrthiknair <herbolax77@gmail.com>
2025-04-26 15:59:37 +0530

    complete UI landing page

--- stremerz-v1/src/App.tsx
+++ stremerz-v1/src/App.tsx
@@ -1,6 +1,11 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
+import { Works } from './components/Works'
+import { Features } from './components/Features'
+import { Price } from './components/Price'
+import { Testimonial } from './components/Testimonial'
+import { Footer } from './components/Footer'
 
 function App() {
 
@@ -8,6 +13,11 @@ function App() {
     <div className='text-neutral-300 text-sm antialiased'>
       <Navbar />
       <Hero />
+      <Works />
+      <Features />
+      <Price />
+      <Testimonial />
+      <Footer />
 
     </div>
   )
